849886d1d4ab6ad3b9b17350e5db12aa9eb1de46
emideepak <79080929+emideepak@users.noreply.github.com>
2021-02-16 11:59:25 +0530

    Delete Changedetection.m

--- Changedetection.m
+++
@@ -1,6 +0,0 @@
-% For identifying the changes happened on coconut trees 
-A = imread('Beforemap.jpg');
-B = imread('Aftermap.jpg');
-imshowpair(A,B,'diff')
-imshow(C)
-imwrite(C,'Final_Change_Map.png');
